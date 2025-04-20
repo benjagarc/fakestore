ac10d237ac374b5c020820de4bc07ba1bc603ee7
bgarciabtk <bgarcia@btkfarma.com>
2025-04-20 10:47:34 -0600

    set mulisegment test

--- components/molecules/Breadcrumb/__test__/Breadcrumb.test.tsx
+++ components/molecules/Breadcrumb/__test__/Breadcrumb.test.tsx
@@ -17,9 +17,30 @@ describe("Breadcrumb", () => {
     expect(homeLink).toHaveAttribute("href", "/");
   });
 
-  it("Should render category ", () => {
+  it("Should render current route correctly", () => {
+    render(<Breadcrumb url={mockUrlCategory} />);
+    const categoryElement = screen.getByText("men's%20clothing");
+    expect(categoryElement).toHaveClass("active");
+    expect(categoryElement).toHaveClass("breadcrumb-item");
+  });
+
+  it("Should render link category correctly", () => {
     render(<Breadcrumb url={mockUrlProduct} />);
     const categoryLink = screen.getByRole("link", { name: "men's%20clothing" });
     expect(categoryLink).toHaveAttribute("href", "/category/men's%20clothing");
+  });
+
+  it("Should render multiple segments correctly", () => {
+    render(<Breadcrumb url={mockUrlProduct} />);
+    const homeLink = screen.getByRole("link", { name: "Home" });
+    expect(homeLink).toHaveAttribute("href", "/");
+    const categoryLink = screen.getByRole("link", { name: "men's%20clothing" });
+    expect(categoryLink).toHaveAttribute("href", "/category/men's%20clothing");
+    const productElement = screen.getByText(
+      "Fjallraven Foldsack No. 1 Backpack, Fits 15 Laptops"
+    );
+    expect(productElement).toBeInTheDocument();
+    expect(productElement).toHaveClass("active");
+    expect(productElement).toHaveClass("breadcrumb-item");
   });
 });
