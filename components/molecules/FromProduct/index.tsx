9d79f6d601ad44783c360eb5639969f649ad421c
bgarciabtk <bgarcia@btkfarma.com>
2025-04-19 18:22:42 -0600

    update bootstrap imports

--- components/molecules/FromProduct/index.tsx
+++ components/molecules/FromProduct/index.tsx
@@ -1,12 +1,7 @@
 import { FC, memo, useState } from "react";
 import { Formik, Form } from "formik";
 import { FormProductType } from "./types";
-import Button from "react-bootstrap/esm/Button";
-import FormGroup from "react-bootstrap/FormGroup";
-import FormLabel from "react-bootstrap/FormLabel";
-import FormControl from "react-bootstrap/FormControl";
-import FormSelect from "react-bootstrap/FormSelect";
-import Spinner from "react-bootstrap/esm/Spinner";
+import { Button, Form as FormBt, Spinner } from "react-bootstrap";
 
 export const FromProducts: FC<FormProductType> = ({
   product,
@@ -31,9 +26,9 @@ export const FromProducts: FC<FormProductType> = ({
       >
         {({ values, errors, touched, handleChange, handleBlur }) => (
           <Form>
-            <FormGroup className="mb-3">
-              <FormLabel htmlFor="title">Title</FormLabel>
-              <FormControl
+            <FormBt.Group className="mb-3">
+              <FormBt.Label htmlFor="title">Title</FormBt.Label>
+              <FormBt.Control
                 id="title"
                 name="title"
                 value={values.title}
@@ -41,14 +36,14 @@ export const FromProducts: FC<FormProductType> = ({
                 onBlur={handleBlur}
                 isInvalid={touched.title && !!errors.title}
               />
-              <FormControl.Feedback type="invalid">
+              <FormBt.Control.Feedback type="invalid">
                 {errors.title}
-              </FormControl.Feedback>
-            </FormGroup>
+              </FormBt.Control.Feedback>
+            </FormBt.Group>
 
-            <FormGroup className="mb-3">
-              <FormLabel htmlFor="price">Price</FormLabel>
-              <FormControl
+            <FormBt.Group className="mb-3">
+              <FormBt.Label htmlFor="price">Price</FormBt.Label>
+              <FormBt.Control
                 id="price"
                 name="price"
                 type="number"
@@ -57,14 +52,14 @@ export const FromProducts: FC<FormProductType> = ({
                 onBlur={handleBlur}
                 isInvalid={touched.price && !!errors.price}
               />
-              <FormControl.Feedback type="invalid">
+              <FormBt.Control.Feedback type="invalid">
                 {errors.price}
-              </FormControl.Feedback>
-            </FormGroup>
+              </FormBt.Control.Feedback>
+            </FormBt.Group>
 
-            <FormGroup className="mb-3">
-              <FormLabel htmlFor="description">Description</FormLabel>
-              <FormControl
+            <FormBt.Group className="mb-3">
+              <FormBt.Label htmlFor="description">Description</FormBt.Label>
+              <FormBt.Control
                 id="description"
                 as="textarea"
                 name="description"
@@ -74,14 +69,14 @@ export const FromProducts: FC<FormProductType> = ({
                 onBlur={handleBlur}
                 isInvalid={touched.description && !!errors.description}
               />
-              <FormControl.Feedback type="invalid">
+              <FormBt.Control.Feedback type="invalid">
                 {errors.description}
-              </FormControl.Feedback>
-            </FormGroup>
+              </FormBt.Control.Feedback>
+            </FormBt.Group>
 
-            <FormGroup className="mb-3">
-              <FormLabel htmlFor="category">Category</FormLabel>
-              <FormSelect
+            <FormBt.Group className="mb-3">
+              <FormBt.Label htmlFor="category">Category</FormBt.Label>
+              <FormBt.Select
                 id="category"
                 name="category"
                 value={values.category}
@@ -94,15 +89,15 @@ export const FromProducts: FC<FormProductType> = ({
                     {category}
                   </option>
                 ))}
-              </FormSelect>
-              <FormControl.Feedback type="invalid">
+              </FormBt.Select>
+              <FormBt.Control.Feedback type="invalid">
                 {errors.category}
-              </FormControl.Feedback>
-            </FormGroup>
+              </FormBt.Control.Feedback>
+            </FormBt.Group>
 
-            <FormGroup className="mb-3">
-              <FormLabel htmlFor="image">Image (URL)</FormLabel>
-              <FormControl
+            <FormBt.Group className="mb-3">
+              <FormBt.Label htmlFor="image">Image (URL)</FormBt.Label>
+              <FormBt.Control
                 id="image"
                 name="image"
                 value={values.image}
@@ -110,10 +105,10 @@ export const FromProducts: FC<FormProductType> = ({
                 onBlur={handleBlur}
                 isInvalid={touched.image && !!errors.image}
               />
-              <FormControl.Feedback type="invalid">
+              <FormBt.Control.Feedback type="invalid">
                 {errors.image}
-              </FormControl.Feedback>
-            </FormGroup>
+              </FormBt.Control.Feedback>
+            </FormBt.Group>
 
             <div className="d-flex justify-content-end gap-2">
               <Button disabled={loading} variant="secondary" onClick={onClose}>
