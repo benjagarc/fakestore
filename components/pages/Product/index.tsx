54b807807411df780d03d253a1e87205a239e88f
bgarciabtk <bgarcia@btkfarma.com>
2025-04-20 19:31:11 -0600

    set message to product does not exist

--- components/pages/Product/index.tsx
+++ components/pages/Product/index.tsx
@@ -4,6 +4,8 @@ import Breadcrumb from "@/components/molecules/Breadcrumb";
 import ProductInformation from "@/components/organism/ProductInformation";
 import { useProducts } from "@/components/reducers/Products";
 import { Container } from "react-bootstrap";
+import styles from "./index.module.scss";
+import Link from "next/link";
 
 export const PageProduct = ({ id }: { id: number | string }) => {
   const { products } = useProducts();
@@ -11,10 +13,17 @@ export const PageProduct = ({ id }: { id: number | string }) => {
   const [product] = products.filter((product) => product.id === +id);
   return (
     <>
-      {product?.title && (
+      {product?.title ? (
         <Container className="py-4">
           <Breadcrumb url={`/category/${product?.category}/${product.title}`} />
           <ProductInformation {...product} />
+        </Container>
+      ) : (
+        <Container className={`card-custom d-flex ${styles.productError}`}>
+          <h1>Product does not exist</h1>
+          <div>
+            <Link href={"/"}>Go to home</Link>
+          </div>
         </Container>
       )}
     </>
